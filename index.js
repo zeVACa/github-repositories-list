6109d0139e784afd02b6d22b9a2bbfe8f5f63f12
Nikita Logunyonok <38005134+zeVACa@users.noreply.github.com>
2022-09-19 21:22:26 +0700

    debounce v2 + render list

--- index.js
+++ index.js
@@ -1,43 +1,37 @@
-// fetch('https://api.github.com/search/repositories?q=tetris+language:assembly&sort=stars&order=desc')
-//   .then((res) => res.json())
-//   .then((data) => console.log(data.items));
+let input = document.querySelector('.input');
+let suggestionsList = document.querySelector('ul.suggestions');
 
-// fetch('https://api.github.com/search/repositories?q=calculator')
-//   .then((res) => res.json())
-//   .then((data) => console.log(data.items));
+let debouncedGetRepositories = debounce(getRepositories, 500);
 
-// fetch('https://api.github.com/search/repositories?q=calculator&page=1&per_page=100')
-//   .then((res) => res.json())
-//   .then((data) => console.log(data));
+input.addEventListener('input', () => debouncedGetRepositories(input.value));
 
-document.querySelector('.input').addEventListener('input', (e) => {
-  // console.log(e.currentTarget.value);
-  const debouncedGetRepositories = debounce(getRepositories, 500);
-  // const debounced = debounce(getRepositories, 500);
-
-  debouncedGetRepositories(e.currentTarget.value);
-  // debounced();
-});
-
-async function getRepositories(inputValue) {
-  let response = await fetch(
-    `https://api.github.com/search/repositories?q=${inputValue}&page=1&per_page=100`,
+async function getRepositories(inputSearchQuery = '') {
+  const REPOSITORIES_PER_PAGE = 5;
+  const response = await fetch(
+    `https://api.github.com/search/repositories?q=${inputSearchQuery}&page=1&per_page=${REPOSITORIES_PER_PAGE}`,
   );
-  let data = await response.json();
+  const data = await response.json();
 
   console.log(data);
+
+  suggestionsList.innerHTML = '';
+
+  console.log(data.items.slice(0, REPOSITORIES_PER_PAGE));
+  data.items.slice(0, 5).forEach((item) => {
+    console.log(item);
+    let li = document.createElement('li');
+
+    li.textContent = item.name;
+    suggestionsList.appendChild(li);
+  });
 }
 
-function debounce(func, timeoutMs) {
-  return function perform(...args) {
-    let previousCall = this.lastCall;
-
-    this.lastCall = Date.now();
-
-    if (previousCall && this.lastCall - previousCall <= timeoutMs) {
-      clearTimeout(this.lastCallTimer);
-    }
-
-    this.lastCallTimer = setTimeout(() => func(...args), timeoutMs);
+function debounce(fn, debounceTime) {
+  let timer;
+  return function () {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn.apply(this, arguments), debounceTime);
+    console.log(timer);
+    console.log('timer');
   };
 }
